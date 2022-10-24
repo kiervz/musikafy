d059771bdef0b7359d6934f7463d4675e3b00758
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2022-10-25 01:38:39 +0800

    custom scroll bar

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => {
         <Sidebar />
         <div className="flex-1 flex flex-col bg-gradient-to-r from-slate-700 to-emerald-800">
           <Searchbar />
-          <div className="px-6 h-[calc(100vh-25px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+          <div className="px-6 h-[calc(100vh-24px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
             <div className="flex-1 h-fit pb-40">
               <Routes>
                 <Route path="/" element={<Discover />} />
